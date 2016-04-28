70bec1a3b5ee98eb006466118c3c3d0ee2862683
Julia Lilith <julia.lilith@gmail.com>
2016-04-27 21:45:44 -0400

    [PROGRAMMED] learned to make a small program

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    char Julia_Segura_Aguirre[100];
+    printf("What is your date of birth?\n");
+    gets(Julia_Segura_Aguirre);
+    printf("You typed: ");
+    puts(Julia_Segura_Aguirre);
     return 0;
 }
