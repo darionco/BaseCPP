c4f455fe0d726ccb14ad7a85570b989d1d1722e6
Julia Lilith <julia.lilith@gmail.com>
2016-04-29 19:46:09 -0400

    [LEFT] made my program the way it was before

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main()
     char Julia_Segura_Aguirre[100];
     printf("What is your date of birth?\n");
     gets(Julia_Segura_Aguirre);
-    char Julia_Segura_Aguirre[100]
-    printf("Great!You typed: n/ Is that correct?");
+  
+    printf("You typed: ");
     puts(Julia_Segura_Aguirre);
   
    
