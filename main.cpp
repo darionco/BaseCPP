98834729e5767b090fe52c4a5cb6138a1d5b3738
Julia Lilith <julia.lilith@gmail.com>
2016-05-02 22:31:59 -0400

    [LEARNED] learrned about functions

--- main.cpp
+++ main.cpp
@@ -1,34 +1,15 @@
 #include <stdio.h>
 
-int addTwoIntegers(int two, int one)
+int add2integers(int z, int space)
 {
-	return one + two + one;
-}
-
-int daddyIsAwesome(int paco, int peco, int chico)
-{
-    int a = paco;
-    int b = chico;
-    int c = peco;
-    
-    return a + b + c;
-}
-
-int juliaIsAlsoAwesome(int dario, int selene)
-{
-   return dario - selene;
+    return space - z;
 }
 
 int main()
-{   
-    int julia, dario;
-    printf("Type 2 Numbers\n");
-    printf("1st Number Is: ");
-    scanf("%d", &dario);
-    printf("2nd Number Is: ");
-    scanf("%d", &julia);
-    int Result = julia + dario;
-    printf("This Is Addition. It Equals To %d\n", Result);
+{  
+    int lalo = 10;
+    int Julia = 6;
+    int a = add2integers(Julia, lalo);
+    printf("%d\n", a);
     return 0;
 }
-
