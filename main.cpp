8889d1ddfb91eaa9c49960a50e6235c807bcd98f
Julia Lilith <julia.lilith@gmail.com>
2016-05-03 22:08:00 -0400

    [HOMEWORK] did my homework that was really hard

--- main.cpp
+++ main.cpp
@@ -6,10 +6,41 @@ int add2integers(int z, int space)
 }
 
 int main()
-{  
-    int lalo = 10;
-    int Julia = 6;
-    int a = add2integers(Julia, lalo);
-    printf("%d\n", a);
+{
+    int programing, homework;
+    printf("I am going to do Addition. Please type 2 numbers.\n");
+    printf("The 1st Number Is ");
+    scanf("%d", &programing);
+    printf("The 2nd Number Is ");
+    scanf("%d", &homework);
+    int Result = programing + homework;
+    printf("Your 2 Numbers Equals To %d\n", Result);
+      
+    int a, b, c;
+    printf("I Am Also Going To Do Multiplacation. Please Type 2 Numbers\n");
+    printf("The 1st Number Is ");
+    scanf("%d", &a);
+    printf("The 2nd Number Is ");
+    scanf("%d", &b);
+    c = a * b;
+    printf("Your 2 Numbers Equals To %d\n", c);
+   
+    int p, s, o;    
+    printf("I Also Want To Do Division. Please Type 2 Numbers\n");
+    printf("The 1st Number Is ");
+    scanf("%d", &p);
+    printf("The 2nd Number Is ");
+    scanf("%d", &s);
+    o = p / s;
+    printf("Your 2 Numbers Equals To %d\n", o);
+    
+    int i, h;
+    printf("Finally, I Would Like To Do Subtraction. Please Type 2 Numbers\n");
+    printf("The 1st Number Is ");
+    scanf("%d", &i);
+    printf("The 2nd Number Is ");
+    scanf("%d", &h);
+    Result=i-h;
+    printf("Your 2 Numbers Equals To %d\n", Result);
     return 0;
 }
