bff0296265bd8a3813b897dc1308db73463dd093
Julia Lilith <julia.lilith@gmail.com>
2016-05-10 23:53:39 -0400

    [HOMEWORK] First big assignment!

--- main.cpp
+++ main.cpp
@@ -1,119 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
 
-int add2integers(int z, int space)
+int askForNumber(const char *message)
 {
-    return space - z;
-}
-int write4life(int weee, int deee)
-{
-    return weee + deee;
-}
-int homweork(int pop, int loopy)
-{
-    return pop * loopy;
-}
-int hoolahoop(int hoola, int hoop)
-{
-    return hoola / hoop;
-}
-int myfavesong(int fave, int soon)
-{
-    return fave * soon;
-}
-int Newfunction(int aliSyee, int daRio, int julIa)
-{
-    return (aliSyee + daRio) * julIa;
+    int number;
+    printf("Please type your %s:\n", message);
+    printf("-> ");
+    scanf("%d", &number);
+    return number;
 }
 
-void superDad()
-{   
-    int mathilda, freinds;    
-    printf("this is Multiplacation. type 2 numbers.\n");
-    printf("1st Number: ");
-    scanf("%d", &mathilda);
-    printf("2nd Number: ");
-    scanf("%d", &freinds);
-    int Result = myfavesong(mathilda, freinds);
-    printf("this will Multiply. It Equals to: %d\n", Result);
+void askForManyNumbers(const char *message, int howMany, int *numbers)
+{
+    printf("Please type %s:\n", message);
+    for (int i = 0; i < howMany; ++i)
+    {
+        printf("-> ");
+        scanf("%d", &numbers[i]);
+    }
 }
 
+void printResult(const char *type, int number)
+{
+    printf("Your numbers %s are equal to: %d\n", type, number);
+}
 
 int main()
-{   
-    superDad();
-    //Testing
-    // int programing, homework;
-    // printf("I am going to do Addition. Please type 2 numbers.\n");
-    // printf("The 1st Number Is ");
-    // scanf("%d", &programing);
-    // printf("The 2nd Number Is ");
-    // scanf("%d", &homework);
-    // int Result = write4life(programing, homework);
-    // printf("Your 2 Numbers Equals To %d\n", Result);
-      
-    // int a, b, c;
-    // printf("I Am Also Going To Do Multiplacation. Please Type 2 Numbers\n");
-    // printf("The 1st Number Is ");
-    // scanf("%d", &a);
-    // printf("The 2nd Number Is ");
-    // scanf("%d", &b);
-    // c = homweork(a, b);
-    // printf("Your 2 Numbers Equals To %d\n", c);
-   
-    // int p, s, o;
-    // printf("I Also Want To Do Division. Please Type 2 Numbers\n");
-    // printf("The 1st Number Is ");
-    // scanf("%d", &p);
-    // printf("The 2nd Number Is ");
-    // scanf("%d", &s);
-    // o = hoolahoop(p, s);
-    // printf("Your 2 Numbers Equals To %d\n", o);
+{
+    srand (time(NULL)); // Do not worry about this, but don't delete it
+    int age = askForNumber("age");
     
-    // int i, h;
-    // printf("Finally, I Would Like To Do Subtraction. Please Type 2 Numbers\n");
-    // printf("The 1st Number Is ");
-    // scanf("%d", &i);
-    // printf("The 2nd Number Is ");
-    // scanf("%d", &h);
-    // Result = i - h;
-    // printf("Your 2 Numbers Equals To %d\n", Result);
+    int favoriteNumbers[3];
+    askForManyNumbers("your 3 favorite numbers", 3, favoriteNumbers);
     
-    // printf("Hi Daddy, This Will Be My Bonus Homework!!!\n");
-    // int v, t, l;
-    // printf("For this i will write them myselfs (to make it harder)\n");
-    // v = 16;
-    // t = 80;
-    // l = 5;
-    // Result = v + t + l;
-    // printf("16 + 80 + 5 = %d\n", Result);
-    // printf("The last one was about Addition. This one will be about Division!\n");
-    // int k, q, z;
-    // k = 1;
-    // q = 63;
-    // z = 6;
-    // Result = q / k / z;
-    // printf("63 Divided by 1 Divided by 6 = %d\n", Result);
-    // int y, g, x;
-    // printf("This one is going to be Multiplacation!\n");
-    // y = 10;
-    // g = 1;
-    // x = 6;
-    // Result = 10 * 6 * 1;
-    // printf("10 X 6 X 1 = %d\n", Result);
-    // int u, j, m;
-    // printf("This last one will be Subtraction!\n");
-    // u = 50;
-    // j = 17;
-    // m = 31;
-    // Result = myfavesong(u, j);
-    // printf("50 - 31 - 17 = %d\n", Result);
+    printf("Your age is: %d\n", age);
+    printf("Your 3 favorite numbers are %d %d %d\n", favoriteNumbers[0], favoriteNumbers[1], favoriteNumbers[2]);
     
+    int added = favoriteNumbers[0] + favoriteNumbers[1] + favoriteNumbers[2];
+    int substracted = favoriteNumbers[0] - favoriteNumbers[1] - favoriteNumbers[2];
+    int multiplied = favoriteNumbers[0] * favoriteNumbers[1] * favoriteNumbers[2];
+    int divided = favoriteNumbers[0] / favoriteNumbers[1] / favoriteNumbers[2];
     
-    // int selene, randomname, randomname2;
-    // selene = 7;
-    // randomname = 12;
-    // randomname2 = 2;
-    // int Result = Newfunction(selene, randomname, randomname2);
-    // printf("The awnser is: %d\n", Result);
+    printResult("added", added);
+    printResult("substracted", substracted);
+    printResult("multiplied", multiplied);
+    printResult("divided", divided);
+    
+    int randomFavoriteNumber = favoriteNumbers[rand() % 3];
+    printf("From your favorite numbers, the next lucky one is: %d\n", randomFavoriteNumber);
+    printf("If you play the lottery, use the number %d\n", randomFavoriteNumber * age);
+    
     return 0;
 }
