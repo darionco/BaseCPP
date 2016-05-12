0dbd04bb5caf277efe0d39ece6911186345b7cad
Julia Lilith <julia.lilith@gmail.com>
2016-05-11 22:54:39 -0400

    [HOMEWORK] did 2 of the assesments!

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int askForNumber(const char *message)
 {
     int number;
-    printf("Please type your %s:\n", message);
+    printf("Please type %s:\n", message);
     printf("-> ");
     scanf("%d", &number);
     return number;
@@ -29,25 +29,25 @@ void printResult(const char *type, int number)
 int main()
 {
     srand (time(NULL)); // Do not worry about this, but don't delete it
-    int age = askForNumber("age");
+    int age = askForNumber("how many siblings you have");
     
-    int favoriteNumbers[3];
-    askForManyNumbers("your 3 favorite numbers", 3, favoriteNumbers);
+    int favoriteNumbers[4];
+    askForManyNumbers("your 4 favorite numbers", 4, favoriteNumbers);
     
     printf("Your age is: %d\n", age);
-    printf("Your 3 favorite numbers are %d %d %d\n", favoriteNumbers[0], favoriteNumbers[1], favoriteNumbers[2]);
+    printf("Your 4 favorite numbers are %d %d %d %d\n", favoriteNumbers[0], favoriteNumbers[1], favoriteNumbers[2], favoriteNumbers[3]);
     
-    int added = favoriteNumbers[0] + favoriteNumbers[1] + favoriteNumbers[2];
-    int substracted = favoriteNumbers[0] - favoriteNumbers[1] - favoriteNumbers[2];
-    int multiplied = favoriteNumbers[0] * favoriteNumbers[1] * favoriteNumbers[2];
-    int divided = favoriteNumbers[0] / favoriteNumbers[1] / favoriteNumbers[2];
+    int added = favoriteNumbers[0] + favoriteNumbers[1] + favoriteNumbers[2] + favoriteNumbers[3];
+    int substracted = favoriteNumbers[0] - favoriteNumbers[1] - favoriteNumbers[2] - favoriteNumbers[3];
+    int multiplied = favoriteNumbers[0] * favoriteNumbers[1] * favoriteNumbers[2] * favoriteNumbers[3];
+    int divided = favoriteNumbers[0] / favoriteNumbers[1] / favoriteNumbers[2] / favoriteNumbers[3];
     
     printResult("added", added);
     printResult("substracted", substracted);
     printResult("multiplied", multiplied);
     printResult("divided", divided);
     
-    int randomFavoriteNumber = favoriteNumbers[rand() % 3];
+    int randomFavoriteNumber = favoriteNumbers[rand() % 4];
     printf("From your favorite numbers, the next lucky one is: %d\n", randomFavoriteNumber);
     printf("If you play the lottery, use the number %d\n", randomFavoriteNumber * age);
     
