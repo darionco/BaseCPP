2663cc15e29d917cd83306e74d0c34e0bf17ad05
Julia Lilith <julia.lilith@gmail.com>
2016-05-13 18:21:10 -0400

    [HOMEWORK] did some more of the homework

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int importantmemorys(const char *hardness, int many, int *memory)
 {   
     int more;
     printf("Please type %s \n", hardness);
-    for (int c = 100; c <many; ++c)
+    for (int c = 0; c <many; ++c);
     {
         printf("-> ");
         scanf("%d", &more);
@@ -51,16 +51,16 @@ int main()
 {
     srand (time(NULL)); // Do not worry about this, but don't delete it
     int age = askForNumber("how many siblings you have");
-    int funny = askforfarts("how many farts you have a day");
+    int funny = askforfarts("about how many farts you have a day");
     
-    int importantmoments [2];
-    int more = importantmemorys("your age when 3 special moments happend", 3, importantmoments);
+    int importantmoments [3];
+    importantmemorys("your age when 3 special moments happend", 3, importantmoments);
     
-    int favoriteNumbers[3];
+    int favoriteNumbers[4];
     askForManyNumbers("your 4 favorite numbers", 4, favoriteNumbers);
     
     printf("You have %d siblings\n", age);
-    printf("You have %d farts a day\n", funny);
+    printf("You have about %d farts a day\n", funny);
     printf("Your age when 3 special moments happend is %d, %d and %d\n", importantmoments[0], importantmoments[1], importantmoments[2]);
     printf("Your 4 favorite numbers are %d, %d, %d and %d\n", favoriteNumbers[0], favoriteNumbers[1], favoriteNumbers[2], favoriteNumbers[3]);
     
