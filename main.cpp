a55f8402fdf29f2b8ef69e24579740b5944e3d27
Julia Lilith <julia.lilith@gmail.com>
2016-05-14 22:24:39 -0400

    [HOMEWORK] left off the rest for tommorow

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ int importantmemorys(const char *hardness, int many, int *memory)
 {   
     int more;
     printf("Please type %s \n", hardness); //NOT WORKING... AT ALL!!!
-    for (int c = 0; c <many; ++c)
-    {
+    {   
+        for (int i = 0; i < many; ++i)
         printf("-> ");
         scanf("%d", &more);
     }
@@ -61,7 +61,7 @@ int main()
     importantmemorys("3 ages of an important time in your life", 3, importantmoments);
     
     printf("You have %d siblings\n", age);
-    printf("Your age when 3 special moments happend is %d, %d and %d\n", favoriteNumbers[0], favoriteNumbers[1], favoriteNumbers[2]);
+    printf("Your age when 3 special moments happend is %d, %d and %d\n", importantmoments[0], importantmoments[1], importantmoments[2]);
     printf("Your 4 favorite numbers are %d, %d, %d and %d\n", favoriteNumbers[0], favoriteNumbers[1], favoriteNumbers[2], favoriteNumbers[3]);
     
     int added = favoriteNumbers[0] + favoriteNumbers[1] + favoriteNumbers[2] + favoriteNumbers[3];
@@ -75,8 +75,11 @@ int main()
     printResult("divided", divided);
     
     int randomFavoriteNumber = favoriteNumbers[rand() % 4];
-    printf("From your favorite numbers, the next lucky one is: %d\n", randomFavoriteNumber);
+    printf("(This is really funny, don't really do any of these, they are just for fun!)\nFrom your favorite numbers, the next lucky one is: %d\n", randomFavoriteNumber);
     printf("If you play the lottery, use the number %d\n", randomFavoriteNumber * age);
+    printf("If your friends ask you to pick a number in between 1 and 1000, use %d\n", randomFavoriteNumber * added);
+    printf("If your playing man hunt and your friend is playing eechi peachi, they pick you, count clock-wise around everyone this many times, %d\n", randomFavoriteNumber + multiplied);
+    printf("if you need to tell someone in 20 years, how old will you be, say your %d!\n", randomFavoriteNumber + substracted);
     
     return 0;
 }
